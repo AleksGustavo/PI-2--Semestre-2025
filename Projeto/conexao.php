544454829dbb7c2c33560cb8958ea333a1359324
Aleksander Gustavo <alekshomeoffice@gmail.com>
2025-12-06 13:36:17 -0300

    Commit da conexão apenas.

--- Projeto/conexao.php
+++ Projeto/conexao.php
@@ -2,10 +2,10 @@
 // Arquivo: conexao.php - Conexão Dupla (MySQLi para $conexao e PDO para $pdo)
 
 $host = 'localhost'; 
-$user = 'u579326255_g5';
-$pass = '*8AcZp6dmS'; 
-$banco = 'u579326255_g5';  
-$port = 3307; // Adicionado para uso opcional, mas essencial para o PDO DSN.
+$user = 'root';
+$pass = ''; 
+$banco = 'petshop_db';  
+$port = 3306; // Adicionado para uso opcional, mas essencial para o PDO DSN.
 
 // --- 1. CONEXÃO MySQLi 
 $conexao = @mysqli_connect($host, $user, $pass, $banco, $port); 
